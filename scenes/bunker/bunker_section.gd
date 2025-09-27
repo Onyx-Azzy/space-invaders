23eaa4b357a6e5e51dd0fd803fc027844ec58846
Onyx-Azzy <onyxazzy@gmail.com>
2025-09-27 22:41:11 +0100

    Bunkers
    Added destructible bunkers

---
+++ scenes/bunker/bunker_section.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var sprite_frame := 0
+
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	$Sprite2D.frame = sprite_frame
+
+
+func _on_area_entered(area: Node2D) -> void:
+	if area.is_in_group("bullet"):
+		area.queue_free()
+		queue_free()
